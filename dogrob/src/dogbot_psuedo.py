08d806c554e0196e4e6389941969f4a18d462b46
Brenna <kinder2@wisc.edu>
2021-04-20 21:57:19 -0500

    Just writing random notes to try to get a ball rolling.

--- dogrob/src/dogbot_psuedo.py
+++ dogrob/src/dogbot_psuedo.py
@@ -1,1 +1,19 @@
-# Write psuedo code for dogrob features.
+#!/usr/bin/env python3
+
+
+# Write psuedo code/random notes for dogrob features.
+
+
+# Import stuff like access to mobrob and servos?
+import rospy
+import traceback
+import numpy as np
+
+# TODO: add mobrob_util messages to dogrob_util
+#from mobrob_util.msg import ME439WaypointXY, ME439PathSpecs
+from geometry_msgs.msg import Pose2D
+from std_msgs.msg import Bool
+
+# We don't want to copy all the mobrob stuff over.
+
+# Make wag_tail a Bool
